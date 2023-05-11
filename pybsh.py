87776acf9308fe05cabf624e20c7dca52bce0302
Ben Rosebery <benrosebery@gmail.com>
2023-05-10 21:11:05 -0400

    upd

--- pybsh.py
+++ pybsh.py
@@ -1,3 +1,11 @@
 ## this is a python implementation of bsh which is based in haskell. Everything is different
 # but the concepts are the sane.
 
+class Bsh():
+    def __init__(self) -> None:
+        pass
+
+    def initBshTable(self):
+        
+        
+
